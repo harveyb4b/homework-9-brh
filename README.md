19abaa6f3d5cb157cb47b91ce7a0669248fa5158
Brian Harvey <harveyb4b@yahoo.com>
2020-08-05 20:10:49 -0400

    adding homework

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# homework-9-brh
+# How to write a great ReadMe 
+Well, this is a great time to create one 
+
+
+## Installation
+details
+
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Questions? Contact me:
+GitHub Username: harveyb4b
+Email: harveyb4b@yahoo.com
---
+++ index.js
@@ -0,0 +1,121 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+// array of questions for user
+const questions = [
+// Title, Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+{
+    type: "input",
+    message: "What is Your GitHub Username?",
+    name: "username"
+},
+{
+    type: "input",
+    message: "What is your Email Address?",
+    name: "email"
+},
+{
+    type: "input",
+    message: "What is Your Github Project Title?",
+    name: "title"
+},
+{
+    type: "input",
+    message: "Give Your Project a Description?",
+    name: "description"
+},
+{
+    type: "list",
+    message: "Table of Contents",
+    name: "toc",
+    choices: [
+        'Yes','No'
+    ]
+},
+{
+    type: "input",
+    message: "Installation",
+    name: "installation"
+},
+{
+    type: "input",
+    message: "Usage",
+    name: "usage"
+},
+{
+    type: "input",
+    message: "Credits",
+    name: "credits"
+},
+{
+    type: "list",
+    message: "License",
+    name: "license",
+    choices:[
+        'MIT','APACHE 2.0','GPL 3.0','BSD 3','None'
+    ]
+},
+{
+    type: "input",
+    message: "Tests",
+    name: "tests"
+}
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+    let {username, email, title, description, toc, installation, usage, credits, license, tests} = data;
+    let fileData = `# ${title} \n${description} \n\n`;
+    let titles = ['Installation','Usage','Credits', 'Tests'];
+    let options = [installation, usage, credits, tests];
+    if(toc === 'Yes'){
+        fileData += "## Table of Contents\n";
+        titles.forEach((x,index) => {
+            if(options[index].length > 0){
+                fileData += `* [${x}](#${x.toLowerCase()}) \n`;
+            }
+        })
+        fileData += '* [Questions](#questions) \n';
+    }
+    fileData += '\n';
+    for(let i = 0; i < options.length; i++){
+        (options[i].length > 0) ? fileData += `## ${titles[i]}\n${options[i]}\n\n` : null;
+    }
+    fileData += `## License\n`;
+    switch(license) {
+        case 'MIT':
+          fileData += '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n\n';
+          break;
+        case 'APACHE 2.0':
+          fileData += '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)\n\n';
+          break;
+        case 'GPL 3.0':
+          fileData += '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)\n\n';
+          break;
+        case 'BSD 3':
+          fileData += '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)\n\n';
+          break;
+        default:
+          // code block
+      }
+    if(username.length > 0 || email.length > 0){
+        fileData += "## Questions? Contact me:\n";
+        (username.length > 0) ? fileData += `GitHub Username: ${username}\n` : null;
+        (email.length > 0) ? fileData += `Email: ${email}\n` : null;
+    }
+    fs.writeFile(fileName, fileData, function(err){
+        console.log(fileData);
+        if(err){
+            console.log(err)
+        }
+    })
+}
+
+// function to initialize program
+function init() {
+    inquirer
+    .prompt(questions)
+    .then( response => writeToFile("README.md", response))
+}
+
+// function call to initialize program
+init();
